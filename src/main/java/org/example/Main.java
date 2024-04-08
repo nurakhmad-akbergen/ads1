14e4381beba57327c0d86e17cd32fe720b9eb816
Nurakhmad Akbergen <nurahmad.akbergen@mail.ru>
2024-04-09 00:13:15 +0500

    second task done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int num_of_task = sc.nextInt();
         switch (num_of_task) {
-            case 1: task_1();
+            case 1:
+                task_1();
                 break;
             case 2:
-
+                task_2();
+            case 3:
+                task_3();
 
         }
     }
@@ -47,5 +50,32 @@ public class Main {
         }
         return min;
     }
+
+    public static void task_2(){
+        double total = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a size of array:");
+        int size = sc.nextInt();
+
+        System.out.println("Enter elements of array:");
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < size; i++){
+            total += arr[i];
+        }
+        double result = total / size;
+
+        System.out.println("Average: " + result);
+    }
+
+    public static void task_3(){
+
+    }
 }
+
 
