5f30debb0cbe51867d76f41373f05a445ea91328
Nurakhmad Akbergen <nurahmad.akbergen@mail.ru>
2024-04-13 00:34:30 +0500

    adding comments to each task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,16 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-
+    /**
+     * Here we just choose a tasks that we need.
+     * To realize it I used switch case.
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num_of_task = sc.nextInt();
         switch (num_of_task) {
             case 1:
                 task_1();
-                break;
             case 2:
                 task_2();
             case 3:
@@ -30,7 +32,10 @@ public class Main {
                 task_10();
         }
     }
-
+    /**
+     * In this task we accept input values from user.
+     * Next with a loop we fill our array and path them on findMin function.
+     */
     public static void task_1() {
         Scanner sc = new Scanner(System.in);
 
@@ -53,7 +58,10 @@ public class Main {
 
         }
     }
-
+    /**
+     * This function find minimum value.
+     * It checks if next value is bigger than curren, if not current value stays.
+     */
     public static int findMin(int size, int[] arr) {
         int min = arr[0];
         for (int i = 0; i < size; i++) {
@@ -63,7 +71,10 @@ public class Main {
         }
         return min;
     }
-
+    /**
+     * In this task we fill array and with a loop plussing all values in one variable total.
+     * Then just deviating it by size and it is important hold it in double type for accuracy.
+     */
     public static void task_2(){
         double total = 0;
         Scanner sc = new Scanner(System.in);
@@ -85,7 +96,9 @@ public class Main {
 
         System.out.println("Average: " + result);
     }
-
+    /**
+     * All logic of this task in PrimeOrNComposite() here we just accept values and call it.
+     */
     public static void task_3(){
         Scanner sc = new Scanner(System.in);
 
@@ -99,7 +112,10 @@ public class Main {
         }
 
     }
-
+    /**
+     * Here we with if statement check if our number deviating by other number except 1 and itself.
+     * We use recursion for checking all number until number itself.
+     */
     public static boolean PrimeOrNComposite(int n, int i){
         if (n == i){
             return true;
@@ -112,7 +128,9 @@ public class Main {
         }
         return PrimeOrNComposite(n, i + 1);
     }
-
+    /**
+     * Here we also just accept values and calling recursion function.
+     */
     public static void task_4(){
         Scanner sc = new Scanner(System.in);
 
@@ -121,16 +139,20 @@ public class Main {
 
         System.out.println(factorial(n));
     }
-
+    /**
+     * In this function we use recursion.
+     * Firstly before each calling we check if n == 0 because we have to be ensured when recursion should be stopped.
+     * Recall just contains itself and next iteration in reversed queue.
+     */
     public static int factorial(int n){
         if (n == 0){
             return 1;
         }
-
-
         return n * factorial(n - 1);
     }
-
+    /**
+     * Just accept and calling recursion function.
+     */
     public static void task_5(){
         Scanner sc = new Scanner(System.in);
 
@@ -139,7 +161,9 @@ public class Main {
 
         System.out.println(fib(n));
     }
-
+    /**
+     * In task, we already know a formula to define a last number of fibonachi.
+     */
     public static int fib(int n){
         if (n <= 1){
             return n;
@@ -147,7 +171,9 @@ public class Main {
             return fib(n - 1) + fib(n - 2);
         }
     }
-
+    /**
+     * Just accept and calling recursion function.
+     */
     public static void task_6(){
         Scanner sc = new Scanner(System.in);
 
@@ -158,14 +184,19 @@ public class Main {
 
         System.out.println(findDegree(n, d));
     }
-
+    /**
+     * If degree number became equal to 0 we return 1 and stop recursion.
+     * Until d == 0 we recall function and each iteration smaller on 1 and then just multiple all them.
+     */
     public static int findDegree(int n, int d){
         if (d == 0){
             return 1;
         }
         return n * findDegree(n, d - 1);
     }
-
+    /**
+     * Just accept and calling recursion function.
+     */
     public static void task_7(){
         Scanner sc = new Scanner(System.in);
 
@@ -180,14 +211,18 @@ public class Main {
         }
         reverseArr(arr, size - 1);
     }
-
+    /**
+     * Recall function with -1 parameter and if i >= 0 we're outputting a value in reverse order.
+     */
     public static void reverseArr(int[] arr, int i){
         if (i >= 0){
             System.out.print(arr[i] + " ");
             reverseArr(arr, i - 1);
         }
     }
-
+    /**
+     * Just accept and calling recursion function.If return True we output "Yes" else "No".
+     */
     public static void task_8(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
@@ -198,6 +233,11 @@ public class Main {
             System.out.println("No");
         }
     }
+    /**
+     * Here we use isDigit for define digits.
+     * In second "if" we check only one digit.
+     * We recall function if string contains more than 1 digit.
+     */
     public static boolean isNumeric(String str) {
         if (str == null) {
             return false;
@@ -207,7 +247,9 @@ public class Main {
         }
         return Character.isDigit(str.charAt(0)) && isNumeric(str.substring(1));
     }
-
+    /**
+     * Just accept and calling recursion function.
+     */
     public static void task_9(){
         Scanner sc = new Scanner(System.in);
 
@@ -220,6 +262,9 @@ public class Main {
         System.out.println(binomialCoefficient(n, k));
 
     }
+    /**
+     * By given formula and recursion we define binomial coefficient.
+     */
     public static int binomialCoefficient(int n, int k){
         if (k == 0 || k == n) {
             return 1;
@@ -227,6 +272,9 @@ public class Main {
             return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
         }
     }
+    /**
+     * Just accept and calling recursion function.
+     */
     public static void task_10(){
         Scanner sc = new Scanner(System.in);
 
@@ -238,7 +286,9 @@ public class Main {
 
         System.out.println(gcd(a, b));
     }
-
+    /**
+     * By given formula and recursion we define binomial coefficient.
+     */
     public static int gcd(int a, int b){
         if (b == 0) {
             return a;
@@ -247,4 +297,3 @@ public class Main {
         }
     }
 }
-
